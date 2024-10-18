4e01410eb5eda6759d1d5a23e6714e7b7cfda772
Brunoluiz19 <brunolp3@al.insper.edu.br>
2024-10-17 21:23:22 -0300

    correção 1

--- programa.py
+++ programa.py
@@ -14,38 +14,22 @@ def main():
         "submarino": 1
     }
 
-    for navio, tamanho in navios_info.items():
-        
-        if navio == "porta-aviões":
-            quantidade = 1
-        elif navio == "navio-tanque":
-            quantidade = 2
-        elif navio == "contratorpedeiro":
-            quantidade = 3
-        elif navio == "submarino":
-            quantidade = 4
-
-        
-        for _ in range(quantidade):
+    for navio, tamanho in tamanhos.items():
+        while True:
             print(f"Insira as informações referentes ao navio {navio} que possui tamanho {tamanho}")
-            while True:
-                linha = int(input("Linha: "))
-                coluna = int(input("Coluna: "))
+            linha = int(input("Linha: "))
+            coluna = int(input("Coluna: "))
+            if navio != "submarino":
+                orientacao = int(input("[1] Vertical [2] Horizontal > "))
+                orientacao_str = "vertical" if orientacao == 1 else "horizontal"
+            else:
+                orientacao_str = "vertical"  # Submarino não precisa de orientação
 
-                
-                if navio == "submarino":
-                    orientacao = "horizontal"
-                else:
-                    orientacao_input = int(input("[1] Vertical [2] Horizontal >"))
-                    orientacao = "vertical" if orientacao_input == 1 else "horizontal"
-
-                
-                if posicao_valida(frota, linha, coluna, orientacao, tamanho):
-                    
-                    preenche_frota(frota, navio, linha, coluna, orientacao, tamanho)
-                    break
-                else:
-                    print("Esta posição não está válida!")
+            if posicao_valida(frota, linha, coluna, orientacao_str, tamanho):
+                preenche_frota(frota, navio, linha, coluna, orientacao_str, tamanho)
+                break
+            else:
+                print("Esta posição não está válida!")
 
     print(frota)
 
